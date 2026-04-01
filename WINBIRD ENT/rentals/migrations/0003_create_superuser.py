ebddb92f845762be68728c5c711c18c4f142e5dc
ademenakelvin <ademenakelvin@gmail.com>
2026-04-01 21:48:31 +0000

    Update and rename 0002_user_is_booking_approver.py to 0003_create_superuser.py

--- WINBIRD ENT/rentals/migrations/0003_create_superuser.py
+++ WINBIRD ENT/rentals/migrations/0003_create_superuser.py
@@ -1,26 +1,23 @@
 from django.db import migrations
 
-
 def create_superuser(apps, schema_editor):
     User = apps.get_model('rentals', 'User')
 
     if not User.objects.filter(username='admin').exists():
         User.objects.create_superuser(
             username='admin',
-            email='lmanful0@gmail.com',
+            email='1manful0@gmail.com',
             password='Admin12345'
         )
-
 
 def remove_superuser(apps, schema_editor):
     User = apps.get_model('rentals', 'User')
     User.objects.filter(username='admin').delete()
 
-
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('rentals', '0001_initial'),
+        ('rentals', '0002_user_is_booking_approver'),
     ]
 
     operations = [
