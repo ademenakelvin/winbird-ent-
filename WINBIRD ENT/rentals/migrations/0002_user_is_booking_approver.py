adc06dfba2437dac2fabe87f03b505fbad01ea8d
ademenakelvin <ademenakelvin@gmail.com>
2026-04-01 21:55:49 +0000

    Update 0002_user_is_booking_approver.py

--- WINBIRD ENT/rentals/migrations/0002_user_is_booking_approver.py
+++ WINBIRD ENT/rentals/migrations/0002_user_is_booking_approver.py
@@ -1,1 +1,15 @@
+from django.db import migrations, models
 
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rentals', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='user',
+            name='is_booking_approver',
+            field=models.BooleanField(default=False),
+        ),
+    ]
