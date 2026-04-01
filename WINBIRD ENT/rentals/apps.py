2fba97fe2ca8f2f7a332e9c19017c4d32ee92c67
ademenakelvin <ademenakelvin@gmail.com>
2026-04-01 21:34:53 +0000

    Update apps.py

--- WINBIRD ENT/rentals/apps.py
+++ WINBIRD ENT/rentals/apps.py
@@ -1,16 +1,6 @@
 from django.apps import AppConfig
 
+
 class RentalsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'rentals'
-
-    def ready(self):
-        from django.contrib.auth import get_user_model
-        User = get_user_model()
-
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='Louis',
-                email='lmanful0@gmail.com',
-                password='Agnes22?'
-            )
