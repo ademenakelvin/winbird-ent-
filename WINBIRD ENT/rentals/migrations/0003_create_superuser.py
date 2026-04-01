950134e63570b8403fb6cd79ec316623fc66af49
ademenakelvin <ademenakelvin@gmail.com>
2026-04-01 23:31:24 +0000

    Update superuser creation logic in migration

--- WINBIRD ENT/rentals/migrations/0003_create_superuser.py
+++ WINBIRD ENT/rentals/migrations/0003_create_superuser.py
@@ -1,29 +1,34 @@
 from django.db import migrations
+from django.contrib.auth.hashers import make_password
+
 
 def create_superuser(apps, schema_editor):
     User = apps.get_model("rentals", "User")
 
     existing_user = User.objects.filter(username="admin").first()
+
     if existing_user:
-        existing_user.set_password("Admin12345")
+        existing_user.password = make_password("Admin12345")
         existing_user.is_superuser = True
         existing_user.is_staff = True
         existing_user.is_active = True
+        existing_user.email = "1manful0@gmail.com"
         existing_user.save()
     else:
-        user = User.objects.create(
+        User.objects.create(
             username="admin",
             email="1manful0@gmail.com",
+            password=make_password("Admin12345"),
             is_superuser=True,
             is_staff=True,
             is_active=True,
         )
-        user.set_password("Admin12345")
-        user.save()
+
 
 def remove_superuser(apps, schema_editor):
     User = apps.get_model("rentals", "User")
     User.objects.filter(username="admin").delete()
+
 
 class Migration(migrations.Migration):
 
