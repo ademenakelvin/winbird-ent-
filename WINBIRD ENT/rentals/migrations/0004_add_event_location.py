1f9eba50cffa2d8c3914bbf12b270e79688cb31f
ademenakelvin <ademenakelvin@gmail.com>
2026-04-02 12:34:23 +0000

    Update 0004_add_event_location.py

--- WINBIRD ENT/rentals/migrations/0004_add_event_location.py
+++ WINBIRD ENT/rentals/migrations/0004_add_event_location.py
@@ -1,1 +1,31 @@
+from django.db import migrations, models
 
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("rentals", "0003_create_superuser"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="booking",
+            name="event_location",
+            field=models.CharField(blank=True, max_length=255),
+        ),
+        migrations.AddField(
+            model_name="customer",
+            name="location",
+            field=models.CharField(blank=True, max_length=255),
+        ),
+        migrations.AddField(
+            model_name="customer",
+            name="emergency_contact_name",
+            field=models.CharField(blank=True, max_length=150),
+        ),
+        migrations.AddField(
+            model_name="customer",
+            name="emergency_contact_phone",
+            field=models.CharField(blank=True, max_length=30),
+        ),
+    ]
