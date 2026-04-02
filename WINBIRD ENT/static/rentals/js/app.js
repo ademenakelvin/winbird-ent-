7bd8cabcef36d2d3bd3505b12d1b9ceb19f20e11
ademenakelvin <ademenakelvin@gmail.com>
2026-04-02 11:13:39 +0000

    Update app.js

--- WINBIRD ENT/static/rentals/js/app.js
+++ WINBIRD ENT/static/rentals/js/app.js
@@ -133,3 +133,13 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+document.querySelectorAll("textarea").forEach((textarea) => {
+    const resize = () => {
+        textarea.style.height = "auto";
+        textarea.style.height = textarea.scrollHeight + "px";
+    };
+
+    textarea.addEventListener("input", resize);
+    resize();
+});
